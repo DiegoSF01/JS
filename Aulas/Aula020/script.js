6ac223851eae4b8606b1c159be27e5b3e416e1b5
Diego de Souza Furlanetto <diegosouzafurlanetto@gmail.com>
2025-05-20 10:50:30 -0300

    JS

--- Aulas/Aula020/script.js
+++ Aulas/Aula020/script.js
@@ -13,6 +13,40 @@ document.querySelector("#titulo");
 
 function clicou(){
     alert("Clicou no botão!");
+    
+    const teste = document.querySelector('.teste');
+    console.log(teste.children[0].children);
+
+    const ul = teste.querySelector('ul');
+    console.log(ul.innerHTML);
+    console.log(ul.innerText);
+    console.log(ul.outerHTML);
+
+    ul.innerHTML = '<li>d</li>';
+
+    console.log(ul.innerHTML);
+    console.log(ul.innerText);
+
+    ul.innerHTML += '<li>e</li>';
+
+    console.log(ul.innerHTML);
+    console.log(ul.innerText);
+
+    console.log(ul.children[0].innerText);
+
+    ul.children[0].innerText = 'a';
+    ul.children[0].innerText += 'a';
+
+
+
+    ul.children[0].append('(alterado)');
+    
+    let newli = document.createElement('li');
+    newli.innerText = 'novo li';
+
+    // adiciona no final
+    // ul.append(newli);
+    ul.prepend(newli);
 }
 
 let botao = document.querySelector(".botao2");
