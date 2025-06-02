3641bfe249605b91c1ab61f8b3aa159f020a1b72
Diego de Souza Furlanetto <diegosouzafurlanetto@gmail.com>
2025-06-02 11:36:32 -0300

    JS

--- Aulas/Aula033/script.js
+++ Aulas/Aula033/script.js
@@ -2,9 +2,18 @@ function clicou(){
     fetch('https://jsonplaceholder.typicode.com/posts')
         .then((response) => {
             return response.json();
-        }).then((json) => {
-            console.log(json[2].title)
         })
+        .then((json) => {
+            console.log(json[2]);
+        })
+        .catch((error) => {
+            console.log('Deu Problema!');
+            console.log(error)
+        })
+        .finally(() => {
+            console.log('Acabou tudo!');
+        })
+
 }
 
 document.querySelector('#botao').addEventListener('click', () => {
